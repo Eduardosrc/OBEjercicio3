5367337e7790fd7d0cc1b38c0370702afe56d734
Eduardosrc <eduardosrc37@gmail.com>
2022-09-09 15:21:52 -0500

    añadido Persona

--- Ejercicio.java
+++ Ejercicio.java
@@ -1,1 +1,15 @@
+public class Ejercicio{
 
+    public static void main(String[] args){
+        Persona persona = new Persona();
+
+        persona.setNombre("Eduardo");
+        persona.setEdad(21);
+        persona.setTelefono("123456789");
+        
+        System.out.println("Nombre: " + persona.getNombre());
+        System.out.println("Edad: " + persona.getEdad());
+        System.out.println("Teléfono: " + persona.getTelefono());
+    }
+
+}
